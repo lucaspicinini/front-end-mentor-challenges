1fdd330f982de83657d78d2c09e0a852756fadb3
lucaspicinini <137008535+lucaspicinini@users.noreply.github.com>
2023-07-19 00:01:52 -0300

    span output finished

--- testing/script001.js
+++ testing/script001.js
@@ -7,27 +7,29 @@ const _elements = {
 };
 
 _elements.buttons.forEach(button => {
-    button.addEventListener("click", e => {
-        const all = _elements.buttons;
-        all.forEach(btn => {
+    button.addEventListener("click", () => {
+        _elements.buttons.forEach(btn => {
             btn.classList.remove("card__numbersactive")
         });
-        const btn = e.target;
-        btn.classList.add("card__numbersactive");
+        button.classList.add("card__numbersactive");
     });
 });
 
-_elements.submitbtn.addEventListener("click", sub => {
+_elements.submitbtn.addEventListener("click", () => {
     const nums = _elements.buttons;
+    let span = _elements.span;
+    let buttonId = 0;
     let c = 0;
     for (i = 0; i < nums.length; i++) {
         if (nums[i].classList.contains("card__numbersactive") === true) {
             c += 1;
+            buttonId = nums[i].textContent;
         };
     };
     if (c === 1) {
         _elements.card1.style.display = "none";
         _elements.card2.style.display = "flex";
+        span.innerHTML = buttonId;
     } else {
         window.alert("Please, select an option.")
     };
